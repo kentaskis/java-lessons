534e69f03ea7742c16bd11ff5d7d17c7d4c0296c
Alexey Lavrov <alexey.l@diffco.us>
2022-10-13 09:15:18 +0200

    some class

---
+++ src/_11102022/Car.java
@@ -0,0 +1,39 @@
+package _11102022;
+
+public class Car {
+    private String name;
+    private String color;
+    private int weight;
+
+    public Car(String name, String color, int weight) {
+        this.name = name;
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+}
+
